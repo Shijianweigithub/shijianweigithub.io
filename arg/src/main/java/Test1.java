c35bf535cac804510846b20b5c7091fa340bf760
shijianwei <shijianweiyx@gmail.com>
2023-12-09 17:34:56 +0800

    ceshi++++

--- arg/src/main/java/Test1.java
+++ arg/src/main/java/Test1.java
@@ -1,2 +1,10 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Test1 {
+    public static void main(String[] args) {
+        int []ans=new int[2];
+        Set<Integer> set=new HashSet<Integer>();
+        set.toArray()
+    }
 }
